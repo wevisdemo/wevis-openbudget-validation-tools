c1bd8e3c2a128e6341ec909610623ec092c836ec
patiphan2000 <srichai.patiphan@gmail.com>
2026-08-13 22:54:25 +0700

    feat: add mock example for tree corrector

--- scripts/_mock_validation.py
+++ scripts/_mock_validation.py
@@ -9,5 +9,13 @@ def main():
     validated_tree = budget_tree.get_validate_tree()
     validated_tree.to_csv('output/validated.csv', index=False)
     
+    # Mock auto correct hierarchy
+    mock_df = pd.read_csv("data/mock_tree_hierarchy.csv")
+    budget_tree = BudgetTree(mock_df)
+    budget_tree.auto_correct_tree()
+    
+    corrected_tree = budget_tree.get_budget_tree()
+    corrected_tree.to_csv('output/corrected.csv', index=False)
+    
 if __name__ == "__main__":
     main()
