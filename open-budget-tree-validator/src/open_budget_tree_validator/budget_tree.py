1b7aace737f9345c0573126c705e9184f026aa79
patiphan2000 <srichai.patiphan@gmail.com>
2026-08-14 19:14:14 +0700

    feat(skeleton-generator): merge generated skeleton with budget tree

--- open-budget-tree-validator/src/open_budget_tree_validator/budget_tree.py
+++ open-budget-tree-validator/src/open_budget_tree_validator/budget_tree.py
@@ -1,9 +1,10 @@
+from typing import List
 import re
 import pandas as pd
 from .tree_validator import validate_and_add_error_message
 from .tree_auto_corrector import correct_budget_tree
 from .utilities import clean_budget_tree, add_depth_and_text, get_closest_match
-from .skeleton_generator import generate_skeleton_from_df
+from .skeleton_generator import generate_skeleton_from_df, SkeletonBudget
 
 class BudgetTree():
     def __init__(
@@ -71,7 +72,8 @@ class BudgetTree():
         if self.budget_bureau_df is None:
             return self.budget_tree
         
-        budget_tree = self.get_budget_tree()
+        self.auto_correct_budgetary_unit_names()
+        budget_tree = self.budget_tree
         
         skeletons = generate_skeleton_from_df(self.budget_bureau_df)
         skeleton_df = pd.concat(
@@ -84,7 +86,52 @@ class BudgetTree():
         ]] = ''
         skeleton_df = skeleton_df[self.get_budget_tree().columns]
         
-        # TODO
         # Fill in any missing plan with skeleton
-        
-        return skeleton_df
+        # Split data into chunks of budgetary unit
+        chunk_ids = (budget_tree['budget_type'] == 'BUDGETARY_UNIT').cumsum()
+        processed_chunks = []
+        for _unit_id, unit_chunk in budget_tree.groupby(chunk_ids):
+            if _unit_id == 0: # skip MINISTRY row
+                processed_chunks.append(unit_chunk)
+                continue
+            
+            budget_unit_name = unit_chunk.head(1)['_text'].values[0]
+            # Split data into chunk of budget_plan
+            plan_chunk_ids = (unit_chunk['budget_type'] == 'BUDGET_PLAN').cumsum()
+            budgetary_unit_chunks: List[pd.DataFrame] = []
+            for _plan_id, plan_chunk in unit_chunk.groupby(plan_chunk_ids):
+                budgetary_unit_chunks.append(plan_chunk)
+                if _plan_id == 0: # skip BUDGETARY UNIT row
+                    continue
+                plan_name = plan_chunk.head(1)['_text'].values[0]
+                # Check for 7.1 to skip
+                if re.search(r"^7\.1", plan_name):
+                    continue
+                
+                # Clean plan name
+                plan_name = re.sub(r"7\.\d+\s?", "", plan_name).strip()
+
+                plan_skeletons = [
+                    sk for sk in skeletons \
+                        if sk.budgetary_unit == budget_unit_name\
+                            and sk.budget_plan == plan_name
+                ]
+                output_names = plan_chunk[plan_chunk['budget_type'].isin(['OUTPUT', 'PROJECT'])]['_text'].values
+                
+                missing_outputs = [
+                    sk for sk in plan_skeletons if sk.get_output_text() not in output_names
+                ]    
+                print(missing_outputs)
+                budgetary_unit_chunks.extend([
+                    sk.get_skeleton_tree() for sk in missing_outputs
+                ])
+            processed_chunks.append(pd.concat(
+                budgetary_unit_chunks,
+                ignore_index=True
+            ))
+            
+        self.budget_tree = pd.concat(
+            processed_chunks,
+            ignore_index=True
+        )
+        return self.budget_tree
